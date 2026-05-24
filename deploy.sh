6c3e122fb12a7faa94dee1ec9b06c93bda0355a9
tvonessen <vonessen@mailbox.org>
2026-05-24 15:35:59 +0200

    chore: enhance deployment script to use staging directory for safer updates

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
 DEST=live
+STAGE="${DEST}.new"
 
-cp -a .next/standalone/. "$DEST"
-cp -a .next/static "$DEST/.next/static"
-cp -a public "$DEST/public"
-cp -a data "$DEST/data"
-cp .env "$DEST/.env"
+rm -rf "$STAGE"
+mkdir -p "$STAGE/.next"
+
+cp -a .next/standalone/. "$STAGE"
+cp -a .next/static "$STAGE/.next/static"
+cp -a public "$STAGE/public"
+cp -a data "$STAGE/data"
+cp .env "$STAGE/.env"
 
 # Stoppe alten Server
 pkill -9 -f next
+
+rm -rf "$DEST"
+mv "$STAGE" "$DEST"
 
 # Starte neuen Server
 set -a
